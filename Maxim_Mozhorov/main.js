dda7721be2fb5d3e3b8f9d7d957ef46cf5c3db2e
max <meison98@bk.ru>
2019-04-20 16:40:26 +0300

    Переделал версию с бургерами

--- Maxim_Mozhorov/main.js
+++ Maxim_Mozhorov/main.js
@@ -1,37 +1,49 @@
 function Order() {
+    this.items = [];
     this.price = 0;
     this.kcal = 0;
+
 }
 
-Order.prototype.AddSmallBurger = function (){
-    this.price +=50;
-    this.kcal +=20;
+Order.prototype.AddBurger = function (size, stuffing) {
+    var Burger = {
+        name: "Burger",
+        size : size,
+        stuffing : stuffing,
+    };
+
+    switch (size) {
+        case "small":
+            this.price += 40;
+            this.kcal += 50;
+            break;
+        case "big":
+            this.price += 100;
+            this.kcal += 80;
+            break;
+    }
+
+    switch (stuffing) {
+        case "cheese":
+            this.price += 15;
+            this.kcal += 10;
+            break;
+        case "salad":
+            this.price += 25;
+            this.kcal += 30;
+            break;
+        case "potate":
+            this.price += 7;
+            this.kcal += 12;
+            break;
+    }
+    this.items.push(Burger);
 };
 
-Order.prototype.AddBigBurger = function (){
-    this.price +=100;
-    this.kcal +=40;
-};
 
-Order.prototype.AddCesar = function (){
-    this.price +=100;
-    this.kcal +=20;
-};
 
-Order.prototype.AddOlive = function (){
-    this.price +=50;
-    this.kcal +=80;
-};
 
-Order.prototype.AddCola = function () {
-    this.price += 50;
-    this.kcal += 40;
-}
 
-Order.prototype.AddCoffee = function () {
-    this.price += 80;
-    this.kcal += 20;
-}
 
 
 var my = new Order();
