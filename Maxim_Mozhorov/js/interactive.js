a9db17b1b02ba5498ea868d1c8fd2257f6cfb65f
max <meison98@bk.ru>
2019-04-22 14:12:08 +0300

    сделал все кроме оформления заказа

--- Maxim_Mozhorov/js/interactive.js
+++ Maxim_Mozhorov/js/interactive.js
@@ -1,28 +1,55 @@
 $(".menu-list_burger .btn").click(function () {
     var size = $('.menu-list_burger .burger-type-class input[type=radio]:checked').val();
-    var stuffing = $('.menu-list_burger .burger-stuffing-type-class input[type=radio]:checked').val();;
+    var stuffing = $('.menu-list_burger .burger-stuffing-type-class input[type=radio]:checked').val();
     MyOrder.AddBurger(size, stuffing);
     if(stuffing === ""){
-        $(".order-list ul").append("<li>Гамбургер("+size+") без дополнительных ингридиентов</li>");
+        $(".order-list ul").append("<li>Гамбургер("+size+") без дополнительных ингридиентов <span data-id='"+(MyOrder.items.length-1)+"' class='glyphicon glyphicon-trash' aria-hidden='true' style='cursor: pointer'></span></li>");
     }
     else {
-        $(".order-list ul").append("<li>Гамбургер("+size+") c "+stuffing+"</li>");
+        $(".order-list ul").append("<li>Гамбургер("+size+") c "+stuffing+" <span data-id='"+(MyOrder.items.length-1)+"' class='glyphicon glyphicon-trash' aria-hidden='true' style='cursor: pointer'></span></li>");
     }
     $(".total-price").html("Итого: "+MyOrder.price + " ("+MyOrder.kcal+" kcal)");
 
 });
 
-
 $(".menu-list_salad .btn").click(function () {
     var type = $('.menu-list_salad input[type=radio]:checked').val();
     MyOrder.AddSalad(type);
-    $(".order-list ul").append("<li>Салат: "+type+"</li>");
+    $(".order-list ul").append("<li>Салат: "+type+" <span data-id='"+(MyOrder.items.length-1)+"' class='glyphicon glyphicon-trash' aria-hidden='true' style='cursor: pointer'></span></li>");
     $(".total-price").html("Итого: "+MyOrder.price + " ("+MyOrder.kcal+" kcal)");
 });
 
 $(".menu-list_drink .btn").click(function () {
     var type = $('.menu-list_drink input[type=radio]:checked').val();
     MyOrder.AddDrink(type);
-    $(".order-list ul").append("<li>Напиток: "+type+"</li>");
+    $(".order-list ul").append("<li>Напиток: "+type+" <span data-id='"+(MyOrder.items.length-1)+"' class='glyphicon glyphicon-trash' aria-hidden='true' style='cursor: pointer'></span></li>");
+    $(".total-price").html("Итого: "+MyOrder.price + " ("+MyOrder.kcal+" kcal)");
+});
+
+
+
+$( ".order-list ul" ).on("click","li .glyphicon-trash", function(event) {
+    MyOrder.deleteFood($(this).attr("data-id"));
+    $( ".order-list ul" ).empty();
+    MyOrder.items.forEach(function (item, i, arr) {
+        if (item.name == "Burger"){
+            var size = item.size;
+            var stuffing = item.stuffing;
+            if(stuffing === ""){
+                $(".order-list ul").append("<li>Гамбургер("+size+") без дополнительных ингридиентов</li>");
+            }
+            else {
+                $(".order-list ul").append("<li>Гамбургер("+size+") c "+stuffing+" <span data-id='"+(MyOrder.items.length-1)+"' class='glyphicon glyphicon-trash' aria-hidden='true' style='cursor: pointer'></span></li>");
+            }
+        }
+        else if (item.name == "Salad"){
+            var type = item.type;
+            $(".order-list ul").append("<li>Салат: "+type+" <span data-id='"+(MyOrder.items.length-1)+"' class='glyphicon glyphicon-trash' aria-hidden='true' style='cursor: pointer'></span></li>");
+        }
+        else if (item.name == "Drink"){
+            var type = item.type;
+            $(".order-list ul").append("<li>Напиток: "+type+" <span data-id='"+(MyOrder.items.length-1)+"' class='glyphicon glyphicon-trash' aria-hidden='true' style='cursor: pointer'></span></li>");
+        }
+    });
     $(".total-price").html("Итого: "+MyOrder.price + " ("+MyOrder.kcal+" kcal)");
 });
--- Maxim_Mozhorov/js/main.js
+++ Maxim_Mozhorov/js/main.js
@@ -32,7 +32,7 @@ Order.prototype.AddBurger = function (size, stuffing) {
             this.price += 20;
             this.kcal += 5;
             break;
-        case "potate":
+        case "potato":
             this.price += 15;
             this.kcal += 10;
             break;
@@ -80,20 +80,69 @@ Order.prototype.AddDrink = function (type) {
 };
 
 
-Order.prototype.getOrderList = function () {
-    console.log("Your price list "+this.price+" tubrics");
-    console.log("Your calories "+this.kcal+" kcal");
-    console.log('Your list of order:');
-    console.log(this.items);
+
+Order.prototype.getInfoItem = function (id) {
+    var item = MyOrder.items[id];
+    var price_item = 0;
+    var kcal_item = 0;
+    if (item.name == "Burger"){
+        if (item.size == "small"){
+            price_item += 50;
+            kcal_item += 20;
+        }
+        else if (item.size == "big") {
+            price_item += 100;
+            kcal_item += 40;
+        }
+        if (item.stuffing != ""){
+            if (item.stuffing == "cheese"){
+                price_item += 10;
+                kcal_item += 20;
+            }
+            else if (item.stuffing == "salad"){
+                price_item += 20;
+                kcal_item += 5;
+            }
+            else if (item.stuffing == "potato"){
+                price_item += 15;
+                kcal_item += 10;
+            }
+        }
+    }
+    else if (item.name == "Salad"){
+        if (item.type == "cesar"){
+            price_item += 100;
+            kcal_item += 20;
+        }
+        else if (item.type == "olive") {
+            price_item += 50;
+            kcal_item += 80;
+        }
+    }
+    else if (item.name == "Drink"){
+        if (item.type == "cola"){
+            price_item += 50;
+            kcal_item += 40;
+        }
+        else if (item.type == "coffee") {
+            price_item += 80;
+            kcal_item += 20;
+        }
+    }
+
+    return [price_item,kcal_item];
 };
 
-Order.prototype.getPrice = function () {
-    return "Your price list "+this.price+" tubrics";
+
+
+Order.prototype.deleteFood = function (id) {
+    debugger;
+    var info_item = MyOrder.getInfoItem(id);
+    MyOrder.price -= info_item[0];
+    MyOrder.kcal -= info_item[1];
+    MyOrder.items.splice(id,1);
 };
 
-Order.prototype.getCalories = function () {
-    return "Your calories "+this.kcal+" kcal";
-};
 
 
 
